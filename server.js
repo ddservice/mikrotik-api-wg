18cfa34db52f62cf2a9ac459c8c773cd010d9fcf
ddservice <ddservice.shop@gmail.com>
2026-06-27 17:30:16 +0700

    Fix missing fs module import for reading WireGuard key

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const crypto = require('crypto');
+const fs = require('fs');
 const db = require('./db');
 const RouterOSClient = require('./routeros');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
